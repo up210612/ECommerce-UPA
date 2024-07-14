86397f363aefb17ba9f230e6539c9ab96cd40dc4
Edgar Sandoval Rodríguez <up210614@alumnos.upa.edu.mx>
2024-07-14 12:47:55 -0600

    checkout page

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Checkout from '../Checkout/Checkout';
 // import Home from '../Home/Home';  Se importa el componente
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
     <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={<Home/>}></Route> */} /* Aqui se usa la ruta
+        <Route path="/checkout" element={<Checkout/>}></Route>
       </Routes>
     </BrowserRouter>
   );
