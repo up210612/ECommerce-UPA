a46316e0186fb62ea5c30f8d0c35a2ac227adfbd
Luis Antonio León Pedroza <up210289@alumnos.upa.edu.mx>
2024-07-15 16:31:38 -0600

    productpage
    first modification to productpage.js with bootstrap

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import React from 'react';
+import ProductPage from '../ProductPage/ProductPage'; // Ajusta la ruta según tu estructura
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const exampleProduct = {
+  title: 'Titulo del producto',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  price: '20.00',
+};
 
 function App() {
   return (
-    <div>
-      
+    <div className="App">
+      <ProductPage product={exampleProduct} />
     </div>
   );
 }
 
 export default App;
+
+
