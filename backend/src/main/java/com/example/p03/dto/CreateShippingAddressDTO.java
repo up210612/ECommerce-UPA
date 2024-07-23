57538fbebccc465acd385b1ebfa541b97a221711
Edgar Sandoval Rodríguez <up210614@alumnos.upa.edu.mx>
2024-07-22 19:12:41 -0600

    Uso de DTOS

---
+++ backend/src/main/java/com/example/p03/dto/CreateShippingAddressDTO.java
@@ -0,0 +1,21 @@
+package com.example.p03.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Schema(description = "Modelo para crear shippingaddress")
+@Data
+public class CreateShippingAddressDTO {
+    
+    private String street;
+
+    private String streetNumber;
+
+    private String apartment;
+
+    private String country;
+
+    private String countyState;
+
+    private Integer zipcode;
+}
---
+++ backend/src/main/java/com/example/p03/mapper/ShippingAddressMapper.java
@@ -0,0 +1,27 @@
+package com.example.p03.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import com.example.p03.model.ShippingAddress;
+import com.example.p03.dto.CreateShippingAddressDTO;
+import com.example.p03.dto.ShippingAddressDTO;
+import java.util.List;
+
+@Mapper(
+  componentModel = "spring", 
+  injectionStrategy = InjectionStrategy.CONSTRUCTOR, 
+  nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
+public interface ShippingAddressMapper {
+
+    ShippingAddressDTO toDTO (ShippingAddress model);
+
+    List<ShippingAddressDTO> toDTO(List<ShippingAddress> model);
+
+    @Mapping(target = "idAddress", ignore = true)
+    ShippingAddress toModel(CreateShippingAddressDTO data);
+    
+}
