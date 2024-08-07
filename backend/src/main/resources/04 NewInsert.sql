f5597a4729d35a82bc27793f2bef562835b71f56
Axel Saucedo <up210486@alumnos.upa.edu.mx>
2024-08-07 15:50:04 -0600

    NewImages

--- backend/src/main/resources/04 NewInsert.sql
+++ backend/src/main/resources/04 NewInsert.sql
@@ -9,12 +9,37 @@ INSERT INTO PRODUCTS (product_name,unit_price, product_description, id_category)
 VALUES ("Jeans Slim Azul Medio", 319, "Jeans slim con pretina sencilla, cinco bolsillos cierre frontal con botón y cremallera.", 1);
 INSERT INTO PRODUCTS (product_name,unit_price, product_description, id_category)
 VALUES ("Jeans Cargo Azul", 349, "Jeans slim cargo con pretina sencilla y bolsillos diagonales.", 1);
+INSERT INTO PRODUCTS (product_name,unit_price, product_description, id_category)
+VALUES ("Jogger Negro ", 349, "Joger de pokemon con bolillos diagonales", 1);
+INSERT INTO PRODUCTS (product_name,unit_price, product_description, id_category)
+VALUES ("Jeans Rotos Negros ", 339, "Jeans slim con pretina sencilla, cierre frontal con botón y cremallera.", 1);
+INSERT INTO PRODUCTS (product_name,unit_price, product_description, id_category)
+VALUES ("Jeans Cargo Negro", 349, "Jeans cargo con bolsillos diagonales.", 1);
+INSERT INTO PRODUCTS (product_name,unit_price, product_description, id_category)
+VALUES ("Jeans Slim Azul Medio", 319, "Jeans slim con pretina sencilla, cinco bolsillos cierre frontal con botón y cremallera.", 1);
+INSERT INTO PRODUCTS (product_name,unit_price, product_description, id_category)
+VALUES ("Jeans Cargo Azul", 349, "Jeans slim cargo con pretina sencilla y bolsillos diagonales.", 1);
+
 
 INSERT INTO PRODUCTIMAGES (id_product, product_image_route) VALUES
-(1,"Images/Jeans/Male/MJeans1/MJeans1Principal.png"),
-(2,"Images/Jeans/Male/MJeans2/MJeans2Principal.png"),
+(1,"Images/Jeans/Male/MJeans1/MJeans1Principal.png");
+(2,"Images/Jeans/Male/MJeans2/MJeans2Principal.png");
 (3,"Images/Jeans/Male/MJeans3/MJeans3Principal.png");
-
-
-
-
+(4,"Images/Jeans/Male/MJeans4/MJeans4Principal.png");
+(4,"Images/Jeans/Male/MJeans4/MJeans4_1.png");
+(4,"Images/Jeans/Male/MJeans4/MJeans4_2.png");
+(5,"Images/Jeans/Male/MJeans5/MJeans5Principal.png");
+(5,"Images/Jeans/Male/MJeans5/MJeans5_1.png");
+(5,"Images/Jeans/Male/MJeans5/MJeans5_2.png");
+(5,"Images/Jeans/Male/MJeans5/MJeans5_3.png");
+(5,"Images/Jeans/Male/MJeans5/MJeans5_4.png");
+(6,"Images/Jeans/Female/FJeans1/FJeans1Principal.png");
+(6,"Images/Jeans/Female/FJeans1/FJeans1_1.png");
+(6,"Images/Jeans/Female/FJeans1/FJeans1_2.png");
+(6,"Images/Jeans/Female/FJeans1/FJeans1_3.png");
+(7,"Images/Jeans/Female/FJeans2/FJeans2Principal.png");
+(8,"Images/Jeans/Female/FJeans3/FJeans3Principal.png");
+(9,"Images/Jeans/Female/FJeans4/FJeans4Principal.png");
+(9,"Images/Jeans/Female/FJeans4/FJeans4_1.png");
+(9,"Images/Jeans/Female/FJeans4/FJeans4_2.png");
+(9,"Images/Jeans/Female/FJeans4/FJeans4_3.png");
