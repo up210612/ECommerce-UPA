8a2072c67ecf1a9dfcabd1fa60e341b81cc4cdb6
Edgar Sandoval Rodríguez <up210614@alumnos.upa.edu.mx>
2024-08-09 16:06:33 -0600

    permitir conexiones

--- backend/src/main/java/com/example/p03/config/WebConfig.java
+++ backend/src/main/java/com/example/p03/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://165.232.129.33:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("Content-Type", "Access-Control-Allow-Headers", "Authorization", "X-Requested-With")
                         .allowCredentials(true);
